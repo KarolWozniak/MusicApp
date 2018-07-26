0b1652f0dc662db53a8c22b1cb632d46d17ed0ee
KarolWozniak <s165308@student.pg.gda.pl>
2018-07-26 13:33:41 +0200

    Adding Adapters.

---
+++ app/src/main/java/com/example/karol/musicapp/Adapter/DataAdapter.java
@@ -0,0 +1,79 @@
+package com.example.karol.musicapp.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.karol.musicapp.MainActivity;
+import com.example.karol.musicapp.R;
+
+import Json.downloadURL;
+
+/**
+ * Created by Karol on 2018-03-27.
+ */
+
+public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder>{
+    private static MainActivity mainActivity;
+    private downloadURL data;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public TextView mTextView;
+        private String Url;
+
+        public ViewHolder(TextView v) {
+            super(v);
+            v.setOnClickListener(this);
+            this.mTextView = v;
+        }
+        public void setUrl(String url)
+        {
+            this.Url=url;
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.i("MainActivity",this.Url);
+            mainActivity.downloadUrl(this.Url);
+        }
+    }
+
+    public DataAdapter(downloadURL data, MainActivity mainActivity) {
+        this.data=data;
+        this.mainActivity=mainActivity;
+    }
+
+    @Override
+    public DataAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.data_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(whichFun(position));
+        holder.setUrl(getUrl(position));
+
+    }
+
+    public  String whichFun(int position)
+    {
+       return "Only one option is available now!!";
+    }
+
+    public String getUrl(int number)
+    {
+        return data.getDownloadURL();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+}
---
+++ app/src/main/java/com/example/karol/musicapp/Adapter/SongsAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.karol.musicapp.Adapter
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.karol.musicapp.PlayerActivity
+import com.example.karol.musicapp.R
+import kotlinx.android.synthetic.main.data_item.view.*
+import java.io.File
+
+class SongsAdapter (val items : ArrayList<File>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.data_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.list_item?.text = items.get(position).name
+        holder?.path = items.get(position).path
+    }
+}
+
+class ViewHolder (view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+    val list_item = view.list_item
+    var path = ""
+
+    override fun onClick(p0: View?) {
+        val context = itemView.context
+        val songIntent = Intent(context, PlayerActivity::class.java)
+        songIntent.putExtra("SONG_PATH", path)
+        context.startActivity(songIntent)
+    }
+
+    init {
+        view.setOnClickListener(this)
+    }
+}
